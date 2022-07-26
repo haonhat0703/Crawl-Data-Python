4581de927f0130d54edafd52d1df152e346b2722
haonhat0703 <haonhat2729@gmail.com>
2022-07-26 12:45:53 +0700

    Crawl data from goctruyentranhhay.com

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import jsons
+from bs4 import BeautifulSoup
+#[]
+links = 'https://goctruyentranhhay.com/api/comic/search/recent?p='
+
+f = open("Data.txt", 'w', encoding = 'utf8')
+
+for index in range(10):
+    resp = requests.get(links + str(index))
+    #soups = BeautifulSoup(resp.content, 'html.parser')
+    #data = jsons.dump(soups)
+    data = json.loads(resp.content)
+    f.write ("PAGE " + str(index+1) + "\n")
+    
+    for name in data["result"]["data"]:
+        f.write(name["name"] + "------" + name["chapterLatest"][0] + "\n")
+    
+    f.write("\n")        
+
+f.close()    
